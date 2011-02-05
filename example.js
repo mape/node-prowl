a487af560e9061377c44987d1ebf5970f4766c27
Mathias Pettersson <mape@mape.me>
2011-02-05 15:55:51 +0100

    Updated module to use new https client API

--- example.js
+++ example.js
@@ -1,12 +1,8 @@
-var sys = require('sys');
 var Prowl = require('prowl');
-
 var prowlApiKey = 'yourApiKey';
-
-process.addListener("uncaughtException", function(exception)
-{
-	sys.log(exception.message)
-	sys.log(exception.stack)
+process.addListener('uncaughtException', function(exception) {
+	console.log(exception.message)
+	console.log(exception.stack)
 
 	/*
 	var exceptionNotification = new Prowl.connection(prowlApiKey);
@@ -20,26 +16,22 @@ process.addListener("uncaughtException", function(exception)
 });
 
 var notification = new Prowl.connection(prowlApiKey);
-
 notification.send({
 	'application': 'node-prowl example' // 256 max -  The name of your application or the application generating the event.
 	,'event': 'Test event' // 1024 max - The name of the event or subject of the notification.
 	,'description': 'Testing Prowl through node.' // 10000 max -  	A description of the event, generally terse.
 	,'priority': 0 /*
 		Default value of 0 if not provided. An integer value ranging [-2, 2] representing:
-
-		  -2. Very Low
-		  -1. Moderate
-		   0. Normal
-		   1. High
-		   2. Emergency
-
+			2. Very Low
+			1. Moderate
+			0. Normal
+			1. High
+			2. Emergency
 		Emergency priority messages may bypass quiet hours according to the user's iPhone app settings.
 	*/
-}, function(info)
-{
-	if (info)
-	{
-		sys.puts(sys.inspect(msg));
+}, function(err, info) {
+	if (err) {
+		throw err;
 	}
+	console.log(info);
 });
